4e9dfd1b4c1a222640337d19b2341c8f88bee602
jack96489 <giacomo.orsenigo@gmail.com>
2021-02-23 18:53:03 +0100

    pr

--- upstream-sync.sh
+++ upstream-sync.sh
@@ -94,7 +94,8 @@ git branch -m "release/${LAST_TAG}"
 git merge "${LAST_TAGGED}"
 git log ${INPUT_GIT_LOG_FORMAT_ARGS}
 git push -u origin "release/${LAST_TAG}"
-
+git log master..release/${LAST_TAG} ${INPUT_GIT_LOG_FORMAT_ARGS}
+gh auth login --with-token ${INPUT_GITHUB_TOKEN}
 gh pr create --title "Release ${LAST_TAG}" --body "$(git log master..release/${LAST_TAG} ${INPUT_GIT_LOG_FORMAT_ARGS})"
 
 
