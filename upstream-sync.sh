b9c8261287630bdc7f6f9450d27aaa4d7ee9fd5d
jack96489 <giacomo.orsenigo@gmail.com>
2021-02-24 10:14:15 +0100

    pr

--- upstream-sync.sh
+++ upstream-sync.sh
@@ -97,7 +97,7 @@ git push -u origin "release/${LAST_TAG}"
 #git log master..release/${LAST_TAG} ${INPUT_GIT_LOG_FORMAT_ARGS}
 echo ${INPUT_GITHUB_TOKEN} | gh auth login --with-token
 #gh auth login status
-gh pr create --title "Release ${LAST_TAG}" # --body "$(git log master..release/${LAST_TAG} ${INPUT_GIT_LOG_FORMAT_ARGS})"
+gh pr create --title "Release ${LAST_TAG}" --fill # --body "$(git log master..release/${LAST_TAG} ${INPUT_GIT_LOG_FORMAT_ARGS})"
 
 
 #if [ "${LOCAL_COMMIT_HASH}" = "${UPSTREAM_COMMIT_HASH}" ]; then
